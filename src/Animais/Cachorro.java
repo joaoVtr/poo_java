c002d868a88a1d2d53b0c496af0afe67f9c4ef5c
joaoVtr <joao_vitor_mj@hotmail.com>
2024-06-11 22:34:12 -0300

    Encapsulamento

--- src/Animais/Cachorro.java
+++ src/Animais/Cachorro.java
@@ -1,12 +1,63 @@
 package Animais;
 
 public class Cachorro {
-    public String nome;
-    public String cor;
-    public int altura;
-    public double peso;
-    public int tamandoRabo;
-    public String estadoEspirito;
+    private String nome;
+    private String cor;
+    private int altura;
+    private double peso;
+    private int tamandoRabo;
+    private String estadoEspirito;
+
+    public Cachorro(){};
+    //Contrutures entre os atributos e os métodos
+    public Cachorro(String nome, String cor, int altura, int tamandoRabo, double peso, String estadoEspirito) {
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.tamandoRabo = tamandoRabo;
+        this.peso = peso;
+        this.estadoEspirito = estadoEspirito;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getTamandoRabo() {
+        return tamandoRabo;
+    }
+
+    public void setTamandoRabo(int tamandoRabo) {
+        this.tamandoRabo = tamandoRabo;
+    }
 
     public void comer(){
         System.out.println("Comer");
@@ -22,6 +73,7 @@ public class Cachorro {
     }
 
     public String interagir(String acao){
+       /*
         if(acao.equals("carinho")){
             this.estadoEspirito = "Feliz";
             return estadoEspirito;
@@ -31,7 +83,15 @@ public class Cachorro {
         else{
             this.estadoEspirito = "neutro";
         }
-        return estadoEspirito;
+        */
+
+        switch (acao){
+            case "carinho": this.estadoEspirito = "Feliz"; break;
+            case "vai dormir": this.estadoEspirito = "bravo"; break;
+            case "pisar na patinha": this.estadoEspirito = "triste"; break;
+            default: this.estadoEspirito = "Neutro";
+        }
+        return this.estadoEspirito;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,37 @@ import Animais.Cachorro;
 public class Main {
     public static void main(String[] args) {
 
-        fcachorro(); 
+        fcachorro();
 
     }
 
     public static void  fcachorro(){
-        Cachorro cachorro1 = new Cachorro();
+//        Cachorro cachorro1 = new Cachorro();
 
+        Cachorro cachorro1 = new Cachorro(
+                "testeDog1","Azul", 1, 2, 2, "teste" );
+        Cachorro cachorro2 = new Cachorro();
+        System.out.println(cachorro1.getNome());
+        System.out.println(cachorro2.getNome());
+        /*
+        // Atributos privatizados
         cachorro1.nome = "TesteDog";
         cachorro1.cor = "Marrom";
         cachorro1.altura= 1;
         cachorro1.peso = 5.5;
-        cachorro1.tamandoRabo = 1;
+        cachorro1.tamandoRabo = 1;*/
+
+        cachorro1.setNome("testeDog");
+        cachorro1.setCor("Azul");
+        cachorro1.setAltura(1);
+        cachorro1.setPeso(1);
 
         System.out.println(cachorro1.pegar());
 
-        System.out.printf(cachorro1.interagir("vai dormir"));
-        System.out.printf(cachorro1.interagir("carinho"));
+        System.out.println(cachorro1.interagir("vai dormir"));
+        System.out.println(cachorro1.interagir("carinho"));
+        System.out.println(cachorro1.interagir("aaaa"));
+        System.out.println(" -- ");
+        System.out.println(cachorro1.getNome());
     }
 }
