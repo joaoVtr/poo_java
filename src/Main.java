59832af0461903e60cdbd8a508e406228faeb714
joaoVtr <joao_vitor_mj@hotmail.com>
2024-06-11 21:31:36 -0300

    Classes nao executaveis

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import Animais.Cachorro;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cachorro cachorro1 = new Cachorro();
+
+        cachorro1.nome = "TesteDog";
+        cachorro1.cor = "Marrom";
+        cachorro1.altura= 1;
+        cachorro1.peso = 5.5;
+        cachorro1.tamandoRabo = 1;
+
+        System.out.println(cachorro1.nome);
     }
 }
